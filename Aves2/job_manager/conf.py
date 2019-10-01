2159002c6cbe6aafbea995d24d09a17d0cacc317
Yang Haibo <yanghaibo1@jd.com>
2019-10-01 20:50:30 +0800

    remove useless conf items

--- Aves2/job_manager/conf.py
+++ Aves2/job_manager/conf.py
@@ -1,27 +1,6 @@
 import os
 
-from k8s_client.k8s_client import k8s_client
 
 
 exec_backend = {
-    'Job': {
-        'create': k8s_client.create_job,
-        'delete': k8s_client.delete_job,
-        'list': k8s_client.list_job,
-    },
-    'ReplicationController': {
-        'create': k8s_client.create_rc,
-        'delete': k8s_client.delete_rc,
-        'list': k8s_client.list_rc,
-    },
-    'Service': {
-        'create': k8s_client.create_svc,
-        'delete': k8s_client.delete_svc,
-        'list': k8s_client.list_svc,
-    },
-    'Ingress': {
-        'create': k8s_client.create_ingress,
-        'delete': k8s_client.delete_ingress,
-        'list': k8s_client.list_ingress
-    }
 }
