0e179e288bf26772b6a94742f31219fca86d477c
Yang Haibo <yanghaibo1@jd.com>
2019-11-06 10:43:33 +0800

    enable admin

--- Aves2/job_manager/admin.py
+++ Aves2/job_manager/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from job_manager.models import AvesJob, K8SWorker
 
 # Register your models here.
+
+class AvesJobAdmin(admin.ModelAdmin):
+    pass
+
+
+class K8SWorkerAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(AvesJob, AvesJobAdmin)
+admin.site.register(K8SWorker, K8SWorkerAdmin)
