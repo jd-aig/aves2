5c11fb26dc9c844a5ccad272828c94c0a8ac08eb
LiJiasheng <lijiasheng12@jd.com>
2020-01-19 18:44:14 +0800

    filter debug

--- Aves2/k8s_manager/filter.py
+++ Aves2/k8s_manager/filter.py
@@ -5,7 +5,7 @@ from .models import User, K8SPvc
 class K8SPvcFilter(django_filters.rest_framework.FilterSet):
     user = django_filters.CharFilter(field_name="user", lookup_expr="exact", method="get_user_pvc")
 
-    def get_user_pvc(self, queryset, name):
+    def get_user_pvc(self, queryset, key, name):
         print("进入get_user_pvc函数" + str(name))
         return queryset.filter(user=User.objects.get(username=name).id)
 
