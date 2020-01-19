c0f4ebdda521c655e02da555669121ca0254bfba
LiJiasheng <lijiasheng12@jd.com>
2020-01-19 18:21:31 +0800

    modify filter class

--- Aves2/k8s_manager/filter.py
+++ Aves2/k8s_manager/filter.py
@@ -3,11 +3,11 @@ from django.db.models import Q
 from .models import User, K8SPvc
 
 class K8SPvcFilter(django_filters.rest_framework.FilterSet):
-    name = django_filters.CharFilter(name="name", lookup_expr="exact")
+    user = django_filters.CharFilter(name="user", lookup_expr="exact")
 
     def get_user_pvc(self, queryset, name):
         return queryset.filter(user=User.objects.get(username=name).id)
 
     class Meta:
         model = K8SPvc
-        fields = ["name"]
+        fields = ["user"]
