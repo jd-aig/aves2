ea1d13d8d152c5a41110ae97cbfcf3caefb01055
LiJiasheng <lijiasheng12@jd.com>
2020-01-19 16:51:01 +0800

    change filterset

--- Aves2/k8s_manager/views.py
+++ Aves2/k8s_manager/views.py
@@ -37,7 +37,7 @@ class K8SPvcViewSet(viewsets.ModelViewSet):
     queryset = K8SPvc.objects.all()
     serializer_class = K8SPvcSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name']
+    filterset_fields = ['user']
 
 class K8SPvcUserRelViewSet(viewsets.ModelViewSet):
     """A ViewSet for K8SPvcUserRel
