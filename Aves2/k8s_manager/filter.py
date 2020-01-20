bdf8f8c7ad45aec0033842a59505c9a5f1ff8d3c
LiJiasheng <lijiasheng12@jd.com>
2020-01-20 14:15:26 +0800

    finish filter pvc

--- Aves2/k8s_manager/filter.py
+++ Aves2/k8s_manager/filter.py
@@ -6,7 +6,6 @@ class K8SPvcFilter(django_filters.rest_framework.FilterSet):
     user = django_filters.CharFilter(field_name="user", lookup_expr="exact", method="get_user_pvc")
 
     def get_user_pvc(self, queryset, key, name):
-        print("进入get_user_pvc函数" + str(name))
         return queryset.filter(user=User.objects.get(username=name).id)
 
     class Meta:
