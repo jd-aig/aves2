c52c8bf774ac98833c1fe56a2257681bb437bc56
Yang Haibo <yanghaibo1@jd.com>
2020-02-24 09:54:53 +0800

    add job_name in AvesJob serializer

--- Aves2/job_manager/serializer.py
+++ Aves2/job_manager/serializer.py
@@ -5,10 +5,14 @@ from .models import AvesJob, K8SWorker
 
 
 class AvesJobSerializer(serializers.ModelSerializer):
+    job_name = serializers.SerializerMethodField()
 
     class Meta:
         model = AvesJob
         fields = '__all__'
+
+    def get_job_name(self, obj):
+        return obj.job_name
 
 
 class K8SWorkerSerializer(serializers.ModelSerializer):
