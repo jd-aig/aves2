ced8ff623bdeaad19248df25c80435e1c9c2b0bd
Yang Haibo <yanghaibo1@jd.com>
2020-02-24 09:54:03 +0800

    update jobmanager admin to show more columns and add filter item

--- Aves2/job_manager/admin.py
+++ Aves2/job_manager/admin.py
@@ -4,11 +4,34 @@ from job_manager.models import AvesJob, K8SWorker
 # Register your models here.
 
 class AvesJobAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'id',
+        'job_name',
+        'username',
+        'namespace',
+        'status',
+        'create_time',
+        'update_time',
+    )
+    list_filter = ('namespace', 'username', 'status')
+    search_fields = ('username', 'name')
+    ordering = ('-id',)
 
 
 class K8SWorkerAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'id',
+        'worker_name',
+        'username',
+        'namespace',
+        'is_main_node',
+        'entrypoint',
+        'cpu_request',
+        'mem_request',
+        'gpu_request',
+    )
+    list_filter = ('namespace', 'username')
+    ordering = ('-id',)
 
 
 admin.site.register(AvesJob, AvesJobAdmin)
