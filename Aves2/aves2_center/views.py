43477bb053ee74bff86f28621de03c86f3f97d46
Yang Haibo <yanghaibo1@jd.com>
2020-03-09 00:09:16 +0800

    fix job order issue

--- Aves2/aves2_center/views.py
+++ Aves2/aves2_center/views.py
@@ -18,7 +18,7 @@ def home(request):
     if request.user.is_active and request.user.is_superuser:
         jobs = AvesJob.objects.all().order_by('-id')
     else:
-        jobs = AvesJob.objects.all().filter(username=request.user.username)
+        jobs = AvesJob.objects.all().filter(username=request.user.username).order_by('-id')
     context = {'jobs': jobs}
     return render(request, 'aves2_center/index.html', context)
 
