f7472248962f5c8fe422287be21003f431d48a77
Yang Haibo <yanghaibo1@jd.com>
2020-03-14 12:13:39 +0800

    api for pai: expose container log information

--- Aves2/job_manager/serializer.py
+++ Aves2/job_manager/serializer.py
@@ -20,3 +20,34 @@ class K8SWorkerSerializer(serializers.ModelSerializer):
     class Meta:
         model = K8SWorker
         fields = '__all__'
+
+
+class WorkerLogSerializer(serializers.ModelSerializer):
+    readlog_url = serializers.SerializerMethodField()
+    loginfo_url = serializers.SerializerMethodField()
+    roleId = serializers.SerializerMethodField()
+    pod_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = K8SWorker
+        fields = ('pod_name', 'readlog_url', 'loginfo_url', 'roleId')
+
+    def get_pod_name(self, obj):
+        return obj.worker_name
+
+    def get_readlog_url(self, obj):
+        url_info = obj.get_container_log_info()
+        if not url_info:
+            return ''
+        else:
+            return url_info[0]
+
+    def get_loginfo_url(self, obj):
+        url_info = obj.get_container_log_info()
+        if not url_info:
+            return ''
+        else:
+            return url_info[1]
+
+    def get_roleId(self, obj):
+        return obj.role_index
